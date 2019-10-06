95dd3676c1f9c92ef3e67da27eea45ead7b612b3
maissa <djibysognane80@gmail.com>
2019-10-06 22:08:16 +0000

    Création d'une instance h1 pour la classe Humain

--- classes.py
+++ classes.py
@@ -1,1 +1,11 @@
-#coding
+#coding:utf-8
+
+class Humain():
+	def __init__(self, nom, prenom, age):
+		self.nom = nom
+		self.prenom = prenom
+		self.age = age
+	def personne(self):
+		print("{} {} a {}ans".float(self.nom, self.prenom, self.age))
+
+h1 = Humain("Michel", "Toma", 30)		
