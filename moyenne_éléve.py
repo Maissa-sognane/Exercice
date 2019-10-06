fd36c1e636d0215bdd7d01c2b99b42bd2bc42021
maissa <djibysognane80@gmail.com>
2019-10-06 22:19:57 +0000

    Programme pour savoir la mention de l'éléve

---
+++ moyenne_éléve.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+moyenne = 14
+
+if 	moyenne >= 10 and moyenne < 12 :  # -> la meme choses 10 <= moyenne <12
+
+	print("Votre moyenne est : ", moyenne ," avec la mention PASSABLE")
+
+elif moyenne	>= 12 and moyenne < 14:
+
+	print("Votre moyenne est : ", moyenne , " avec la mention ASSEZ-BIEN")
+
+elif moyenne	>= 14 and moyenne < 16:
+
+	print("Votre moyenne est : ", moyenne , " avec la mention BIEN")
+
+elif moyenne	>= 16 and moyenne < 18:	
+
+	print("Votre moyenne est : ", moyenne , " avec la mention TRES-BIEN")
+
+elif moyenne >= 18:
+
+	print("Votre moyenne est : ", moyenne , " avec la mention EXCELLENTE")
+
+else :
+
+	 print("Votre moyenne est : ", moyenne , " et vous étes ajourné! COURAGE")
